e039cf29f8c5545b42578042d36acc61df30bb85
saikiran_greemus <saikiranjillella2894@gmail.com>
2024-10-21 14:08:33 +0530

    Modify s.o.p statement and add one s.o.p statement

--- HelloWorldMain.java
+++ HelloWorldMain.java
@@ -5,7 +5,8 @@ public class HelloWorldMain
 	public static void main(String[] args)
 	 {
 		System.out.println("Hello World...."); 
-		System.out.println("Hello Git "); 
+		System.out.println("Hello GitHub "); 
+		System.out.println("Welcome to GitHub "); 
 	}
 
 }
